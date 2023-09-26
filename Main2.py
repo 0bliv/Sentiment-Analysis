f81ae96f0f969b123248290e8b5d94bb77d94c53
0bliv <88675619+0bliv@users.noreply.github.com>
2023-09-26 20:26:05 -0300

    GUI

--- Main2.py
+++ Main2.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+from tkinter import messagebox
 import sqlite3
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -7,77 +9,89 @@ from nltk.probability import FreqDist
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 import string
 import heapq
-import matplotlib.pyplot as plt  # Importe o matplotlib para visualização do gráfico
+import matplotlib.pyplot as plt
 
 nltk.download("punkt")
 nltk.download("vader_lexicon")
 nltk.download("stopwords")
 
-# Entrada do usuário
-user_text = input("Enter the text for sentiment analysis: ")
+# Função para realizar a análise de sentimento
+def analyze_sentiment():
+    user_text = text_input.get("1.0", "end-1c")  # Obter o texto da caixa de entrada
 
-# Texto de exemplo para análise (usado se a entrada do usuário estiver vazia)
-if not user_text.strip():
-    user_text = """
-    As an English learner, you will likely want to tell others that English is not your first language. You will also need to ask native speakers to repeat phrases and words or to speak slower.
-    """
+    if not user_text.strip():
+        messagebox.showinfo("Aviso", "Por favor, insira um texto para análise de sentimento.")
+        return
 
-# Tokenizar o texto em frases e palavras
-sentences = sent_tokenize(user_text)
-words = word_tokenize(user_text)
+    sentences = sent_tokenize(user_text)
+    words = word_tokenize(user_text)
 
-# Remover stopwords e pontuação
-stop_words = set(stopwords.words("english"))
-filtered_words = [word.lower() for word in words if word.isalnum() and word.lower() not in stop_words]
+    stop_words = set(stopwords.words("english"))
+    filtered_words = [word.lower() for word in words if word.isalnum() and word.lower() not in stop_words]
 
-# Análise de Sentimento
-analyzer = SentimentIntensityAnalyzer()
-sentiment_score = analyzer.polarity_scores(user_text)
+    analyzer = SentimentIntensityAnalyzer()
+    sentiment_score = analyzer.polarity_scores(user_text)
 
-# Extração de Palavras-chave (Top 5)
-word_freq = FreqDist(filtered_words)
-top_keywords = heapq.nlargest(5, word_freq, key=word_freq.get)
+    word_freq = FreqDist(filtered_words)
+    top_keywords = heapq.nlargest(5, word_freq, key=word_freq.get)
 
-# Sumarização (Primeiras 2 frases)
-summary = TreebankWordDetokenizer().detokenize(sentences[:2])
+    summary = TreebankWordDetokenizer().detokenize(sentences[:2])
 
-# Armazenar dados no banco de dados SQLite
-conn = sqlite3.connect("analysis_results.db")
-cursor = conn.cursor()
+    conn = sqlite3.connect("analysis_results.db")
+    cursor = conn.cursor()
 
-# Criar uma tabela para armazenar os resultados, se ela não existir
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS analysis_results (
-        sentiment_score REAL,
-        top_keywords TEXT,
-        summary TEXT
-    )
-''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS analysis_results (
+            sentiment_score REAL,
+            top_keywords TEXT,
+            summary TEXT
+        )
+    ''')
 
-# Inserir os resultados no banco de dados
-cursor.execute("INSERT INTO analysis_results (sentiment_score, top_keywords, summary) VALUES (?, ?, ?)",
-               (sentiment_score['compound'], ', '.join(top_keywords), summary))
+    cursor.execute("INSERT INTO analysis_results (sentiment_score, top_keywords, summary) VALUES (?, ?, ?)",
+                   (sentiment_score['compound'], ', '.join(top_keywords), summary))
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
 
-# Exibir Resultados
-print("Sentiment Analysis:")
-print(sentiment_score)
-print("\nTop Keywords:")
-print(top_keywords)
-print("\nSummary:")
-print(summary)
+    result_text.config(state=tk.NORMAL)
+    result_text.delete("1.0", tk.END)
+    result_text.insert(tk.END, "Análise de Sentimento:\n")
+    result_text.insert(tk.END, f"Pontuação: {sentiment_score}\n\n")
+    result_text.insert(tk.END, "Palavras-chave Principais:\n")
+    result_text.insert(tk.END, f"{', '.join(top_keywords)}\n\n")
+    result_text.insert(tk.END, "Resumo:\n")
+    result_text.insert(tk.END, summary)
+    result_text.config(state=tk.DISABLED)
 
-# Opção para mostrar a visualização do gráfico
-show_graph = input("Do you want to show sentiment analysis as a graph? (yes/no): ")
-
-if show_graph.lower() == "yes":
-    # Criar um gráfico de barras para visualizar a pontuação de sentimento
-    labels = ['Negative', 'Neutral', 'Positive', 'Compound']
-    values = [sentiment_score['neg'], sentiment_score['neu'], sentiment_score['pos'], sentiment_score['compound']]
+    show_graph = messagebox.askquestion("Exibir Gráfico", "Deseja exibir a análise de sentimento como um gráfico?")
     
-    plt.bar(labels, values)
-    plt.title('Sentiment Analysis')
-    plt.ylabel('Score')
-    plt.show()
+    if show_graph == "yes":
+        labels = ['Negativo', 'Neutro', 'Positivo', 'Composto']
+        values = [sentiment_score['neg'], sentiment_score['neu'], sentiment_score['pos'], sentiment_score['compound']]
+    
+        plt.bar(labels, values)
+        plt.title('Análise de Sentimento')
+        plt.ylabel('Pontuação')
+        plt.show()
+
+# Configuração da janela
+root = tk.Tk()
+root.title("Análise de Sentimento")
+
+# Caixa de entrada de texto
+text_label = tk.Label(root, text="Insira o texto para análise :")
+text_label.pack()
+
+text_input = tk.Text(root, height=10, width=50)
+text_input.pack()
+
+# Botão de análise
+analyze_button = tk.Button(root, text="Analisar", command=analyze_sentiment)
+analyze_button.pack()
+
+# Resultados
+result_text = tk.Text(root, height=10, width=50, state=tk.DISABLED)
+result_text.pack()
+
+root.mainloop()
